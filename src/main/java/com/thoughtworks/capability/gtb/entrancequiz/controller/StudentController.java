0b549ecf54220d5464b45667012935ac5c683d09
Tingzi123 <1044373556@qq.com>
2020-10-27 21:36:21 +0800

    [ct]:feat group students

--- src/main/java/com/thoughtworks/capability/gtb/entrancequiz/controller/StudentController.java
+++ src/main/java/com/thoughtworks/capability/gtb/entrancequiz/controller/StudentController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -29,4 +30,13 @@ public class StudentController {
         return ResponseEntity.ok().body(students);
     }
 
+    @CrossOrigin
+    @GetMapping(value = "/students/group", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity getAllStudentByGroup() {
+        ArrayList<Student>[] groupStudents= studentService.getAllStudentByGroup();
+        if (groupStudents == null) {
+            return ResponseEntity.badRequest().build();
+        }
+        return ResponseEntity.ok().body(groupStudents);
+    }
 }
--- src/main/java/com/thoughtworks/capability/gtb/entrancequiz/service/StudentService.java
+++ src/main/java/com/thoughtworks/capability/gtb/entrancequiz/service/StudentService.java
@@ -4,9 +4,8 @@ import com.thoughtworks.capability.gtb.entrancequiz.dto.Student;
 import com.thoughtworks.capability.gtb.entrancequiz.repo.StudentRepo;
 import org.springframework.stereotype.Service;
 
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
+import java.lang.reflect.Array;
+import java.util.*;
 
 @Service
 public class StudentService {
@@ -19,14 +18,35 @@ public class StudentService {
     public List<Student> getAllStudent() {
         List<Student> students = studentRepo.getAllStudent();
 
-        if (students.size()!=0){
+        if (students.size() != 0) {
             Collections.sort(students, new Comparator<Student>() {
                 @Override
                 public int compare(Student o1, Student o2) {
-                    return o1.getId()-o2.getId();
+                    return o1.getId() - o2.getId();
                 }
             });
         }
         return students;
+    }
+
+    public ArrayList<Student>[] getAllStudentByGroup() {
+        ArrayList<Student>[] groupStudent = new ArrayList[6];
+
+        List<Student> students = studentRepo.getAllStudent();
+
+        Random random = new Random();
+
+        if (students.size() > 0) {
+            for (Student stu : students) {
+                if (stu.getId() < students.size() / 6 * 6) {
+                    int rand = random.nextInt(6);
+                    groupStudent[rand].add(stu);
+                } else {
+                    int rand = random.nextInt(students.size() % 6);
+                    groupStudent[rand].add(stu);
+                }
+            }
+        }
+        return groupStudent;
     }
 }
